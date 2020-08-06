801202c572acc42f1534a95cceb9a872322db526
tamas.eger <egertamas@gmail.com>
2020-08-06 11:04:01 +0200

    Change item to charfield from integer in storage

--- src/api/storages/models.py
+++ src/api/storages/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Storage(models.Model):
     store_id = models.ForeignKey('stores.Store', on_delete=models.PROTECT)
-    item_id = models.IntegerField()
+    item = models.CharField(max_length=255, default='')
     stock = models.IntegerField(default=0)
